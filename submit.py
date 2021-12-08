465fe4fac3009381964799aacf4377500a89ff2e
aqntks <cjd0030@gmail.com>
2021-12-08 18:00:10 +0900

    제출용 코드

--- submit.py
+++ submit.py
@@ -3,7 +3,7 @@ from tqdm import tqdm
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score
-
+from sklearn.neural_network import MLPClassifier
 import pandas as pd
 import warnings
 warnings.filterwarnings(action='ignore')
@@ -18,7 +18,7 @@ train = train.drop(['id', 'contents_open_dt'], axis=1)
 test = test.drop(['id', 'contents_open_dt'], axis=1)
 
 
-model = RandomForestClassifier(n_estimators=300, max_depth=60, n_jobs=-1)
+model = MLPClassifier(hidden_layer_sizes=(100,), learning_rate_init=0.01, max_iter=300, random_state=11)
 
 x = train.iloc[:, :-1]
 y = train.iloc[:, -1]
@@ -30,4 +30,4 @@ preds = model.predict(test)
 submission = pd.read_csv('sample_submission.csv')
 submission['target'] = preds
 
-submission.to_csv('baseline.csv', index=False)
+submission.to_csv('mlp.csv', index=False)
